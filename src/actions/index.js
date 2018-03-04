1c221a7e0c469dfbe224e324ce7e8b6489ee0a20
paranyak <paranyak@ucu.edu.ua>
2018-03-04 21:38:44 +0200

    added example of lecture 4: Refactoring the Entry Point.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,10 @@
-let nextTodoId = 0;
-export const addTodo = (text) => {
-    return {
-        type: 'ADD_TODO',
-        id: (nextTodoId++).toString(),
-        text,
-    };
-};
+import { v4 } from 'node-uuid'
+
+export const addTodo = (text) => ({
+    type: 'ADD_TODO',
+    id: v4(),
+    text
+});
 
 export const setVisibilityFilter = (filter) => {
     return {
